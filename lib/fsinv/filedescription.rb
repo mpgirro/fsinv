a0f8b7a791cea2ff41d6436707a0142e3449d9d0
Maximilian Irro <max@disposia.org>
2014-07-29 09:36:51 +0200

    refactored constructor; moved calculation of mime/magicdescr/crc32/md5 into private methods, to keep the constructor cleaner

--- lib/fsinv/filedescription.rb
+++ lib/fsinv/filedescription.rb
@@ -14,41 +14,10 @@ module Fsinv
       super(path,reduced_scan)
 
       unless reduced_scan # don't do this if we only want to know file sizes (for pseudofiles, .git folders, etc)
-        begin
-          type_str = MIME::Types.type_for(@path).join(', ')
-          @@mime_tab.add(type_str) unless @@mime_tab.contains?(type_str)
-          @mimetype = @@mime_tab.get_id(type_str)
-        rescue ArgumentError # if this happens you should definitly repair some file names
-          puts "error: mime type unavailable" unless @@options[:silent]
-          @mimetype = nil
-        end
-    
-        begin 
-          description = sanitize_string(@@fmagic.file(@path))
-          @@magic_tab.add(description) unless @@magic_tab.contains?(description)
-          @magicdescr = @@magic_tab.get_id(description)
-        rescue
-          puts "error: file magic file information unavailable" unless @@options[:silent]
-          @magicdescr = nil
-        end
-      
-        if @@options[:crc32]
-          begin
-            @crc32 = Digest::CRC32.file(@path).hexdigest
-          rescue
-            puts "error calculating crc32 for #{path}" if @@options[:verbose]
-            @crc32 = "error during calculation"
-          end
-        end
-
-        if @@options[:md5]
-          begin
-            @md5 = Digest::MD5.file(@path).hexdigest
-          rescue
-            puts "error calculating md5 for #{path}" if @@options[:verbose]
-            @md5 = "error during calculation"
-          end
-        end
+        @mimetype = get_mime_id
+        @magicdescr = get_magic_descr_ids
+        @crc32 = calc_crc32
+        @md5 = calc_md5
       end
     end # initialize
   
@@ -79,14 +48,64 @@ module Fsinv
     def marshal_load(data)
       self.path = data['path']
       self.bytes = data['bytes']
-      self.ctime = data['ctime']
-      self.mtime = data['mtime']
-      self.mimetype = data['mimetype']
-      self.magicdescr = data['magicdescr']
+      self.ctime = data['ctime'] if data['ctime'].exists?
+      self.mtime = data['mtime'] if data['mtime'].exists?
+      self.mimetype = data['mimetype'] if data['mimetype'].exists?
+      self.magicdescr = data['magicdescr'] if data['magicdescr'].exists?
       self.crc32 = data["crc32"] if data['crc32'].exists?
       self.md5 = data["md5"] if data['md5'].exists?
       self.osx_tags = data['osx_tags'] if data['osx_tags'].exists?
       self.fshugo_tags = data['fshugo_tags'] if data['fshugo_tags'].exists?
     end
+    
+    private
+    def get_mime_id
+      begin
+        type_str = MIME::Types.type_for(@path).join(', ')
+        @@mime_tab.add(type_str) unless @@mime_tab.contains?(type_str)
+        return @@mime_tab.get_id(type_str)
+      rescue ArgumentError # if this happens you should definitly repair some file names
+        puts "error: mime type unavailable" unless @@options[:silent]
+        return nil
+      end
+    end
+    
+    private
+    def get_magic_descr_ids
+      begin 
+        description = sanitize_string(@@fmagic.file(@path))
+        @@magic_tab.add(description) unless @@magic_tab.contains?(description)
+        return @@magic_tab.get_id(description)
+      rescue
+        puts "error: file magic file information unavailable" unless @@options[:silent]
+        return nil
+      end
+    end
+    
+    private 
+    def calc_crc32
+      if @@options[:crc32]
+        begin
+          return Digest::CRC32.file(@path).hexdigest
+        rescue
+          puts "error calculating crc32 for #{path}" if @@options[:verbose]
+          return nil
+        end
+      end
+    end
+    
+    
+    private 
+    def calc_md5
+      if @@options[:md5]
+        begin
+          return Digest::MD5.file(@path).hexdigest
+        rescue
+          puts "error calculating md5 for #{path}" if @@options[:verbose]
+          return nil
+        end
+      end
+    end
+    
   end # FileDefinition
 end
